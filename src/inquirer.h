a32e101d5fb29d6af440c670addb9b4fea1f1a05
Donatas Mockus <mockus49@gmail.com>
2023-11-19 14:53:06 +0100

    Inquirer: tidy up the code

--- src/inquirer.h
+++ src/inquirer.h
@@ -18,16 +18,13 @@
 #define CPP_INQUIRER_SRC_INQUIRER_H
 
 #include <iostream>
-#include <unordered_map>
 #include <string>
 #include <utility>
 #include <vector>
 #include <regex>
-#include <variant>
 #include <cassert>
 
 namespace alx {
-
 class Inquirer;
 
 enum class Type
@@ -41,51 +38,55 @@ enum class Type
 	regex = 6
 };
 
-struct Question
+class Question
 {
+public:
 	Question(const Question& q)
 	{
-		key = q.key;
-		question = q.question;
-		answer = q.answer;
-		type = q.type;
-		if (type == Type::options)
-			options = q.options;
-		else if (type == Type::regex)
-			regex = q.regex;
-
+		m_key = q.m_key;
+		m_question = q.m_question;
+		m_answer = q.m_answer;
+		m_type = q.m_type;
+		if (m_type == Type::options)
+			m_options = q.m_options;
+		else if (m_type == Type::regex)
+			m_regex = q.m_regex;
 	}
-	Question(std::string key, std::string question, Type type = Type::text)
-		: key(std::move(key)),
-		  question(std::move(question)),
-		  type(type) {}
+
+	Question(std::string key, std::string question, const Type type = Type::text)
+		: m_key(std::move(key)),
+		  m_question(std::move(question)),
+		  m_type(type) {}
+
 	Question(std::string key, std::string question, std::vector<std::string> options)
-		: key(std::move(key)),
-		  question(std::move(question)),
-		  type(Type::options),
-		  options(std::move(options))
+		: m_key(std::move(key)),
+		  m_question(std::move(question)),
+		  m_type(Type::options),
+		  m_options(std::move(options))
 	{
-		assert(!this->options.empty() && "Must have one or more options");
+		if (m_options.empty())
+			throw std::runtime_error("Must have one or more options");
 	}
 
 	Question(std::string key, std::string question, std::string regex)
-		: key(std::move(key)),
-		  question(std::move(question)),
-		  type(Type::regex),
-		  regex(std::move(regex)) {}
+		: m_key(std::move(key)),
+		  m_question(std::move(question)),
+		  m_type(Type::regex),
+		  m_regex(std::move(regex)) {}
 
 	~Question() {}
 
 private:
 	friend Inquirer;
-	std::string key;
-	std::string question;
-	std::string answer;
-	Type type;
+	std::string m_key;
+	std::string m_question;
+	std::string m_answer;
+	Type m_type;
+
 	union
 	{
-		std::vector<std::string> options;
-		std::string regex;
+		std::vector<std::string> m_options;
+		std::string m_regex;
 	};
 };
 
@@ -93,211 +94,180 @@ class Inquirer
 {
 	std::vector<std::pair<std::string, Question>> m_questions;
 	std::string m_title;
-public:
-	explicit Inquirer(std::string title) : m_title(std::move(title)) {}
 
-	void add_question(const Question& question)
-	{
-		m_questions.emplace_back(question.key, question);
-	}
+public:
+	explicit Inquirer(std::string title)
+		: m_title(std::move(title)) {}
+
+	void add_question(const Question& question) { m_questions.emplace_back(question.m_key, question); }
 
 	void ask()
 	{
 		if (!m_title.empty())
 			std::cout << "\033[34m>\033[0m " << m_title << '\n';
-		for (auto& question : m_questions)
-		{
+		for (auto& question : m_questions) {
 			auto& q = question.second;
 
-			auto printQuestion = [&](const std::string& append = "")
-			{
-			  std::cout << "\033[1m\033[34m?\033[0m \033[1m" << q.question << "\033[0m " << append;
+			auto printQuestion = [&](const std::string& append = "") {
+				std::cout << "\033[1m\033[34m?\033[0m \033[1m" << q.m_question << "\033[0m " << append;
 			};
 
-			auto takeInput = [](std::string& destination)
-			{
-			  std::cout << "\033[34m";
-			  std::getline(std::cin, destination);
-			  std::cout << "\033[0m";
+			auto takeInput = [](std::string& destination) {
+				std::cout << "\033[34m";
+				std::getline(std::cin, destination);
+				std::cout << "\033[0m";
 			};
 
-			switch (q.type)
-			{
-			case Type::confirm:
-			{
+			switch (q.m_type) {
+			case Type::confirm: {
 				printQuestion("(y/N) ");
 				std::string answer;
 				takeInput(answer);
-				while (!(answer == "y" || answer == "Y" || answer == "n" || answer == "N"))
-				{
+				while (!(answer == "y" || answer == "Y" || answer == "n" || answer == "N")) {
 					erase_lines(2);
 					printQuestion("(y/N) ");
 					takeInput(answer);
 				}
-				q.answer = answer;
+				q.m_answer = answer;
 				break;
 			}
 			case Type::text:
 				printQuestion();
-				takeInput(q.answer);
+				takeInput(q.m_answer);
 				break;
-			case Type::integer:
-			{
+			case Type::integer: {
 				printQuestion();
 				std::string answer;
 				takeInput(answer);
-				while (!is_integer(answer))
-				{
+				while (!is_integer(answer)) {
 					erase_lines(2);
 					printQuestion();
 					takeInput(answer);
 				}
-				q.answer = answer;
+				q.m_answer = answer;
 				break;
 			}
-			case Type::decimal:
-			{
+			case Type::decimal: {
 				printQuestion();
 				std::string answer;
 				takeInput(answer);
-				while (!is_decimal(answer))
-				{
+				while (!is_decimal(answer)) {
 					erase_lines(2);
 					printQuestion();
 					takeInput(answer);
 				}
-				q.answer = answer;
+				q.m_answer = answer;
 				break;
 			}
-			case Type::yesNo:
-			{
+			case Type::yesNo: {
 				const std::string yes = "\033[34myes\033[0m no\n";
 				const std::string no = "yes \033[34mno\033[0m\n";
 				std::cout << std::flush;
 				printQuestion(yes);
-				int key;
 				bool position = true;
-				while (true)
-				{
-					key = getch();
-					if (key == 68)
-					{
+				while (true) {
+					const int key = getch();
+					if (key == 68) {
 						position = true;
 						erase_lines(2);
 						printQuestion(yes);
 					}
-					else if (key == 67)
-					{
+					else if (key == 67) {
 						position = false;
 						erase_lines(2);
 						printQuestion(no);
 					}
-					if (key == 13)
-					{
-						q.answer = position ? "yes" : "no";
+					if (key == 13) {
+						q.m_answer = position ? "yes" : "no";
 						break;
 					}
 				}
 				break;
 			}
-			case Type::options:
-			{
-				int key;
+			case Type::options: {
 				unsigned int selectedIndex = 0;
-				auto printOptions = [&]()
-				{
-				  std::cout << '\n';
-				  for (int i = 0; i < q.options.size(); ++i)
-				  {
-					  if (i == selectedIndex)
-						  std::cout << "\033[34m> " << q.options.at(i) << "\033[0m\n";
-					  else
-						  std::cout << "  " << q.options.at(i) << "\n";
-				  }
+				auto printOptions = [&]() {
+					std::cout << '\n';
+					for (int i = 0; i < q.m_options.size(); ++i) {
+						if (i == selectedIndex)
+							std::cout << "\033[34m> " << q.m_options.at(i) << "\033[0m\n";
+						else
+							std::cout << "  " << q.m_options.at(i) << "\n";
+					}
 				};
 				printQuestion();
 				printOptions();
 
-				while (true)
-				{
-					key = getch();
-					if (key == 66)
-					{
-						selectedIndex = wrap_int(selectedIndex + 1, 0, q.options.size() - 1);
-						erase_lines(q.options.size() + 2);
+				while (true) {
+					const int key = getch();
+					if (key == 66) {
+						selectedIndex = wrap_int(selectedIndex + 1, 0, q.m_options.size() - 1);
+						erase_lines(q.m_options.size() + 2);
 						printQuestion();
 						printOptions();
 					}
-					else if (key == 65)
-					{
-						selectedIndex = wrap_int(selectedIndex - 1, 0, q.options.size() - 1);
-						erase_lines(q.options.size() + 2);
+					else if (key == 65) {
+						selectedIndex = wrap_int(selectedIndex - 1, 0, q.m_options.size() - 1);
+						erase_lines(q.m_options.size() + 2);
 						printQuestion();
 						printOptions();
 					}
-					if (key == 13)
-					{
-						q.answer = q.options.at(selectedIndex);
-						erase_lines(q.options.size() + 2);
-						printQuestion("\033[34m" + q.options.at(selectedIndex) + "\033[0m\n");
+					if (key == 13) {
+						q.m_answer = q.m_options.at(selectedIndex);
+						erase_lines(q.m_options.size() + 2);
+						printQuestion("\033[34m" + q.m_options.at(selectedIndex) + "\033[0m\n");
 						break;
 					}
 				}
 				break;
 			}
-			case Type::regex:
-			{
+			case Type::regex: {
 				printQuestion();
 				std::string answer;
 				takeInput(answer);
-				while (!std::regex_match(answer, std::regex(q.regex)))
-				{
+				while (!std::regex_match(answer, std::regex(q.m_regex))) {
 					erase_lines(2);
 					printQuestion();
 					takeInput(answer);
 				}
-				q.answer = answer;
+				q.m_answer = answer;
 				break;
 			}
 			}
-
 		}
-
 	}
 
 	void print_questions() const
 	{
-		for (const auto& q : m_questions)
-		{
-			std::cout << q.second.question << " " << static_cast<int>(q.second.type) << '\n';
+		for (const auto& q : m_questions) {
+			std::cout << q.second.m_question << " " << static_cast<int>(q.second.m_type) << '\n';
 		}
 	}
 
 	void print_answers() const
 	{
 		for (const auto& q : m_questions)
-			std::cout << q.second.question << ": " << q.second.answer << '\n';
+			std::cout << q.second.m_question << ": " << q.second.m_answer << '\n';
 	}
 
 	std::string answer(const std::string& key) const
 	{
 		for (const auto& question : m_questions)
 			if (question.first == key)
-				return question.second.answer;
+				return question.second.m_answer;
 		return "";
 	}
 
 private:
-	static void erase_lines(unsigned int count = 1)
+	static void erase_lines(const unsigned count = 1)
 	{
 		if (count == 0)
 			return;
 
 		std::cout << "\x1b[2K"; // Delete current line
-		for (int i = 1; i < count; ++i)
-		{
+		for (int i = 1; i < count; ++i) {
 			std::cout << "\x1b[1A" // Move cursor one line up
-					  << "\x1b[2K"; // Delete current line
+				<< "\x1b[2K"; // Delete current line
 		}
 		std::cout << '\r';
 	}
@@ -318,9 +288,9 @@ private:
 		return (*p == 0);
 	}
 
-	static unsigned int wrap_int(unsigned int k, const unsigned int lowerBound, const unsigned int upperBound)
+	static unsigned wrap_int(unsigned int k, const unsigned lowerBound, const unsigned upperBound)
 	{
-		unsigned int range_size = upperBound - lowerBound + 1;
+		const unsigned range_size = upperBound - lowerBound + 1;
 
 		if (k < lowerBound)
 			k += range_size * ((lowerBound - k) / range_size + 1);
@@ -330,17 +300,15 @@ private:
 
 	static int getch()
 	{
-		int c; // This function should return the keystroke without allowing it to echo on screen
+		// This function should return the keystroke without allowing it to echo on screen
 
-		system("stty raw");    // Raw input - wait for only a single keystroke
-		system("stty -echo");  // Echo off
-		c = getchar();
+		system("stty raw"); // Raw input - wait for only a single keystroke
+		system("stty -echo"); // Echo off
+		const int c = getchar();
 		system("stty cooked"); // Cooked input - reset
-		system("stty echo");   // Echo on - Reset
+		system("stty echo"); // Echo on - Reset
 		return c;
 	}
-
 };
-
 }
 #endif //CPP_INQUIRER_SRC_INQUIRER_H
